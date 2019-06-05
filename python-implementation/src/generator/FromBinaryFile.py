8bfa83322a7a28a3b6824cc323e628cc211ad4cf
Ewan Gilligan <eg207@st-andrews.ac.uk>
2019-06-05 18:04:00 +0100

    Fixed issue with casting

--- python-implementation/src/generator/FromBinaryFile.py
+++ python-implementation/src/generator/FromBinaryFile.py
@@ -7,21 +7,22 @@ class FromBinaryFile(RNG):
         try:
             self.f = open(filepath, "rb", size * 64)
         except IOError:
+            print("Couldn't open file:", filepath)
             exit(1)
 
     def next_long(self):
-        b0 = 0xFF & int(self.f.read(1))
-        b1 = 0xFF & int(self.f.read(1))
-        b2 = 0xFF & int(self.f.read(1))
-        b3 = 0xFF & int(self.f.read(1))
-        b4 = 0xFF & int(self.f.read(1))
-        b5 = 0xFF & int(self.f.read(1))
-        b6 = 0xFF & int(self.f.read(1))
-        b7 = 0xFF & int(self.f.read(1))
+        b0 = 0xFF & int.from_bytes(self.f.read(1), byteorder="little", signed=False)
+        b1 = 0xFF & int.from_bytes(self.f.read(1), byteorder="little", signed=False)
+        b2 = 0xFF & int.from_bytes(self.f.read(1), byteorder="little", signed=False)
+        b3 = 0xFF & int.from_bytes(self.f.read(1), byteorder="little", signed=False)
+        b4 = 0xFF & int.from_bytes(self.f.read(1), byteorder="little", signed=False)
+        b5 = 0xFF & int.from_bytes(self.f.read(1), byteorder="little", signed=False)
+        b6 = 0xFF & int.from_bytes(self.f.read(1), byteorder="little", signed=False)
+        b7 = 0xFF & int.from_bytes(self.f.read(1), byteorder="little", signed=False)
         return b7 | (b6 << 8) | (b5 << 16) | (b4 << 24) | (b3 << 32) | (b2 << 40) | (b1 << 48) | (b0 << 56)
 
     def next_double(self):
-        return abs(self.next_long() / 2**64)
+        return abs(self.next_long() / 2 ** 64)
 
     def next_64_bits(self):
         return self.next_long()
