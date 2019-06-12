0f8060e132f83c2f992c52520f2e8e5388599d98
Ewan Gilligan <eg207@st-andrews.ac.uk>
2019-06-12 13:33:23 +0100

    removed randology repo

--- randology-dump-master/thesis/src/uk/ac/stir/randology/generator/Randu.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.stir.randology.generator;
-
-import java.math.BigInteger;
-
-public class Randu extends LCG {
-	public Randu(long seed) {
-		super("Randu",65539,0,(new BigInteger("2")).pow(31).longValue(),seed);
-	}
-}
