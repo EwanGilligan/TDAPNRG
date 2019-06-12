d3a6a64e4e64d47b371077a0af0820d499212b68
eg207 <eg207@st-andrews.ac.uk>
2019-06-12 18:14:55 +0100

    Added pythonRandom, which just uses the default python random number generator.

--- python-implementation/src/randology/Experiment1.py
+++ python-implementation/src/randology/Experiment1.py
@@ -7,11 +7,14 @@ import matplotlib
 
 from src.generator.FromBinaryFile import FromBinaryFile
 from src.generator.Randu import Randu
+from src.generator.pythonRandom import pythonRandom
 from src.randology.HypercubeTest import HypercubeTest
 rng = FromBinaryFile("TrueRandom1", 12000)
+python_rand = pythonRandom("Python Random")
 
 randu = Randu(int(time.time()))
-test = HypercubeTest(runs=5, number_of_points=12000, dimension=3, max_simplex_dim=1, filtration_size=50, reference_rng=rng)
-print(test.perform_test(rng))
+test = HypercubeTest(runs=10, number_of_points=12000, dimension=3, max_simplex_dim=0, filtration_size=50, reference_rng=rng)
+print(test.perform_test(randu))
+print(test.perform_test(python_rand))
 
 
--- python-implementation/src/randology/HypercubeTest.py
+++ python-implementation/src/randology/HypercubeTest.py
@@ -8,21 +8,22 @@ from matplotlib import pyplot
 from src.generator import RNG
 
 
-def create_filtration_range(n=10, max_value=0.1):
+def create_filtration_range(n=20, max_value=0.1):
     return np.linspace(0, max_value, n)
 
 
 class HypercubeTest:
 
-    def __init__(self, runs: int, number_of_points: int, dimension: int, max_simplex_dim: int,
-                 filtration_size: int, reference_rng: RNG) -> None:
+    def __init__(self, reference_rng: RNG, number_of_points: int, runs: int = 10, dimension: int = 3,
+                 max_simplex_dim: int = 1,
+                 filtration_size: int = 20, max_filtration_value: int = 0.1) -> None:
         self.runs = runs
         self.number_of_points = number_of_points
         self.dimension = dimension
         self.max_simplex_dim = max_simplex_dim
         # self.filtration_range = np.array([0.015, 0.02, 0.025, 0.03, 0.035, 0.04, 0.045, 0.05, 0.055, 0.06, 0.065, 0.070, 0.075, 0.08, 0.085, 0.09, 0.095])
         # self.filtration_size = len(self.filtration_range)
-        self.filtration_range = create_filtration_range(filtration_size, 0.1)
+        self.filtration_range = create_filtration_range(filtration_size, max_filtration_value)
         self.filtration_size = filtration_size
         self.reference_distribution = self.generate_distribution(reference_rng)
 
@@ -33,7 +34,7 @@ class HypercubeTest:
         return homology[0]
 
     def generate_diagram(self, points: np.array) -> List[np.ndarray]:
-        filtration = Rips(maxdim=self.max_simplex_dim, thresh=self.filtration_range[-1])
+        filtration = Rips(maxdim=self.max_simplex_dim, thresh=self.filtration_range[-1], verbose=False)
         diagrams = filtration.fit_transform(points)
         return diagrams
 
