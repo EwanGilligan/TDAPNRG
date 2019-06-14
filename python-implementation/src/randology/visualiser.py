10bf302949f04d2126df50a3f03e67f9ec9ec350
eg207 <eg207@st-andrews.ac.uk>
2019-06-14 14:06:47 +0100

    Got a basic version of the visualisation of connectedness working
    Still need to find a way to automatically pick epsilon, although this could be done with the persistence diagram.

--- python-implementation/src/randology/visualiser.py
+++ python-implementation/src/randology/visualiser.py
@@ -1,13 +1,21 @@
+import time
+
 import kmapper as km
+from sklearn import cluster
 
 from src.generator.FromBinaryFile import FromBinaryFile
+from src.generator.GameRand import GameRand
 from src.generator.Randu import Randu
-from src.randology.HypercubeTest import HypercubeTest
+from src.randology.HypercubeTest import HypercubeTest, generate_points
+from matplotlib import pyplot
 
-rng = FromBinaryFile("TrueRandom1", 12000)
-test = HypercubeTest(number_of_points=10000, reference_rng=rng)
-datacloud = test.generate_points(Randu(1))
+rng = Randu(int(time.time()))
+#rng = FromBinaryFile("TrueRandom1", 12000)
+#rng = GameRand(int(time.time()))
+datacloud = generate_points(rng, 3, 10000)
 mapper = km.KeplerMapper(verbose=1)
-projected_data = mapper.fit_transform(datacloud, projection=[0, 1])
-graph = mapper.map(projected_data, datacloud, cover=km.Cover(n_cubes = 45))
-mapper.visualize(graph, path_html="not_random.html")
+nerve = km.GraphNerve(10)
+projected_data = mapper.fit_transform(datacloud, projection=[0, 1, 2])
+clusterer = cluster.DBSCAN(eps=0.05)
+graph = mapper.map(projected_data, datacloud, cover=km.Cover(n_cubes=8), nerve=nerve, clusterer=clusterer)
+mapper.visualize(graph, path_html=rng.get_name() + ".html")
