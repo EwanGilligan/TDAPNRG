fa8942e7f6a82c159395589e99d0838d176da1b2
Ewan Gilligan <eg207@st-andrews.ac.uk>
2019-07-01 14:56:02 +0100

    added check in FromBinaryFile that the EOF hasn't been reached.
    This is because read returns an empty string, which will read to zero.

--- python-implementation/src/pnrg/FromBinaryFile.py
+++ python-implementation/src/pnrg/FromBinaryFile.py
@@ -24,7 +24,11 @@ class FromBinaryFile(RNG):
         # b6 = 0xFF & int.from_bytes(self.f.read(1), byteorder="little", signed=False)
         # b7 = 0xFF & int.from_bytes(self.f.read(1), byteorder="little", signed=False)
         # return b7 | (b6 << 8) | (b5 << 16) | (b4 << 24) | (b3 << 32) | (b2 << 40) | (b1 << 48) | (b0 << 56)
-        return np.int64(int.from_bytes(self.f.read(8), byteorder="little", signed=True))
+        byte = self.f.read(8)
+        # checks that the end of the file hasn't been reached.
+        if not byte:
+            raise EOFError
+        return np.int64(int.from_bytes(byte, byteorder="little", signed=True))
 
     def next_float(self):
         return np.float64(abs(self.next_int() / (2**63 - 1)))
