0e6ccf863bf58badbfe4bb33ff7bda94a4f1ed9e
eg207 <eg207@st-andrews.ac.uk>
2019-07-02 11:16:32 +0100

    Reformatted file, added test configuration, and updated gitignore

--- python-implementation/src/pnrg/CSG/GameRand.py
+++ python-implementation/src/pnrg/CSG/GameRand.py
@@ -1,6 +1,7 @@
 from src.pnrg.RNG import RNG
 import numpy as np
 
+
 class GameRand(RNG):
     def __init__(self, seed: int):
         super().__init__("GameRand")
@@ -9,8 +10,8 @@ class GameRand(RNG):
 
     def next_int(self):
         self.high = (self.high << 32) + (self.high >> 32)
-        self.high = (self.high + self.low) % (2**63 - 1)
-        self.low = (self.low + self.high) % (2**63 - 1)
+        self.high = (self.high + self.low) % (2 ** 63 - 1)
+        self.low = (self.low + self.high) % (2 ** 63 - 1)
         return self.high
 
     def next_float(self):
