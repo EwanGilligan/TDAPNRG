ea4536d709bf81fc8097ca3a9c122b260b6df7ea
Ewan Gilligan <eg207@st-andrews.ac.uk>
2019-07-15 16:52:20 +0100

    reverted accidental commenting out of experiment 1

--- python-implementation/Experiment1.py
+++ python-implementation/Experiment1.py
@@ -15,8 +15,6 @@ seed1 = 0xDEADBEEF
 seed2 = 0xD978757273
 
 
-
-
 # rng2 = FromBinaryFile("../random-org-seq/TrueRandom2", 12000)
 # fileRand = FromBinaryFile("../pseudo-random-sequences/outXorShift32", 12000)
 # python_rand = pythonRandom("Python Random")
@@ -28,50 +26,51 @@ seed2 = 0xD978757273
 def get_reference_rng():
     return FromBinaryFile("../random-org-seq/TrueRandom1", 12000)
 
+
 def get_generators():
     return [
         # Linear Congruential Generators:
-        # Randu(seed1),
-        #Randu(seed2),
-       # Minstd(seed1),
-        #Minstd(seed2),
-        # Glibc(seed1),
-        # Glibc(seed2),
+        Randu(seed1),
+        Randu(seed2),
+        Minstd(seed1),
+        Minstd(seed2),
+        Glibc(seed1),
+        Glibc(seed2),
         pythonRandom(seed1),
         pythonRandom(seed2),
-        # FromBinaryFile("../pseudorandom-sequences/outjava.util.Random-seed1", 12000, "java.util.Random"),
-        # FromBinaryFile("../pseudorandom-sequences/outjava.util.Random-seed2", 12000, "java.util.Random"),
-        # FromBinaryFile("../pseudorandom-sequences/outMWCNR-seed1", 12000, "MWCNR"),
-        # FromBinaryFile("../pseudorandom-sequences/outMWCNR-seed2", 12000, "MWCNR"),
-        # EICG1(seed1),
-        # EICG1(seed2),
+        FromBinaryFile("../pseudorandom-sequences/outjava.util.Random-seed1", 12000, "java.util.Random"),
+        FromBinaryFile("../pseudorandom-sequences/outjava.util.Random-seed2", 12000, "java.util.Random"),
+        FromBinaryFile("../pseudorandom-sequences/outMWCNR-seed1", 12000, "MWCNR"),
+        FromBinaryFile("../pseudorandom-sequences/outMWCNR-seed2", 12000, "MWCNR"),
+        EICG1(seed1),
+        EICG1(seed2),
         # Linear Feedback Shift Registers:
-        # LFSR(seed1),
-        # LFSR(seed2),
-        # FromBinaryFile("../pseudorandom-sequences/outXorShift32-seed1", 12000, "XorShift32"),
-        # FromBinaryFile("../pseudorandom-sequences/outXorShift32-seed2", 12000, "XorShift32"),
-        # FromBinaryFile("../pseudorandom-sequences/outXorShift-seed1", 12000, "XorShift64"),
-        # FromBinaryFile("../pseudorandom-sequences/outXorShift-seed2", 12000, "XorShift64"),
+        LFSR(seed1),
+        LFSR(seed2),
+        FromBinaryFile("../pseudorandom-sequences/outXorShift32-seed1", 12000, "XorShift32"),
+        FromBinaryFile("../pseudorandom-sequences/outXorShift32-seed2", 12000, "XorShift32"),
+        FromBinaryFile("../pseudorandom-sequences/outXorShift-seed1", 12000, "XorShift64"),
+        FromBinaryFile("../pseudorandom-sequences/outXorShift-seed2", 12000, "XorShift64"),
         # WELL generators:
-        # FromBinaryFile("../pseudorandom-sequences/outMersenneTwister-seed1", 12000, "MersenneTwister"),
-        # FromBinaryFile("../pseudorandom-sequences/outMersenneTwister-seed2", 12000, "MersenneTwister"),
+        FromBinaryFile("../pseudorandom-sequences/outMersenneTwister-seed1", 12000, "MersenneTwister"),
+        FromBinaryFile("../pseudorandom-sequences/outMersenneTwister-seed2", 12000, "MersenneTwister"),
         # Cryptographically Secure Generators:
-        # FromBinaryFile("../pseudorandom-sequences/outBlumBlumShub-seed1", 12000, "BlumBlubShub"),
-        #FromBinaryFile("../pseudorandom-sequences/outBlumBlumShub-seed2", 12000, "BlumBlubShub"),
-        # QCG631(seed1),
-        # QCG651(seed1),
-        # Webkit2(seed1),
-        # GameRand(seed1),
-        # FromBinaryFile("../pseudorandom-sequences/outPCG32", 12000, "PCG32"),
-        # FromBinaryFile("../pseudorandom-sequences/outRANROT", 12000, "Ranrot"),
-        # FromBinaryFile("../pseudorandom-sequences/outLamar", 12000, "Lamar"),
-        # FromBinaryFile("../pseudorandom-sequences/outRule30", 12000, "Rule30"),
-        # FromBinaryFile("../pseudorandom-sequences/outRANROT", 12000, "Ranrot"),
+        FromBinaryFile("../pseudorandom-sequences/outBlumBlumShub-seed1", 12000, "BlumBlubShub"),
+        FromBinaryFile("../pseudorandom-sequences/outBlumBlumShub-seed2", 12000, "BlumBlubShub"),
+        QCG631(seed1),
+        QCG651(seed1),
+        Webkit2(seed1),
+        GameRand(seed1),
+        FromBinaryFile("../pseudorandom-sequences/outPCG32", 12000, "PCG32"),
+        FromBinaryFile("../pseudorandom-sequences/outRANROT", 12000, "Ranrot"),
+        FromBinaryFile("../pseudorandom-sequences/outLamar", 12000, "Lamar"),
+        FromBinaryFile("../pseudorandom-sequences/outRule30", 12000, "Rule30"),
+        FromBinaryFile("../pseudorandom-sequences/outRANROT", 12000, "Ranrot"),
         # True Random:
-        # FromBinaryFile("../random-org-seq/TrueRandom2", 12000),
-        #FromBinaryFile("../random-org-seq/TrueRandom3", 12000),
+        FromBinaryFile("../random-org-seq/TrueRandom2", 12000),
+        FromBinaryFile("../random-org-seq/TrueRandom3", 12000),
         # Other:
-        #Quasirandom()
+        Quasirandom()
     ]
 
 
@@ -96,4 +95,4 @@ print("Scale:0.15")
 test3.test_generator_list(get_generators())
 print("Scale 0.075")
 test4.test_generator_list(get_generators())
-#test.test_generators_multiple_scales(get_generators(), scale_list=[0.15, 0.45, 1.0], failure_threshold=1)
+# test.test_generators_multiple_scales(get_generators(), scale_list=[0.15, 0.45, 1.0], failure_threshold=1)
