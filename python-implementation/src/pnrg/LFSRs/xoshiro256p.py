fc6be2dcae3f916812511d27defe55873ee19e89
Ewan Gilligan <eg207@st-andrews.ac.uk>
2019-07-17 18:51:09 +0100

    fixed xoroshiro generators.
    needed to cast certain values to unsigned integers.

--- python-implementation/src/pnrg/LFSRs/xoshiro256p.py
+++ python-implementation/src/pnrg/LFSRs/xoshiro256p.py
@@ -2,13 +2,16 @@ import numpy as np
 
 from pnrg import RNG
 
+np.seterr(over='ignore')
 
 class Xoshiro256p(RNG):
     """
     Adapted from http://xoshiro.di.unimi.it/
     """
     # maximum value when calculating floating point values, as only the upper 53 bits are used.
-    max_float = (1 << 53 - 1)
+    max_float = (1 << 53)
+    u_17 = np.uint64(17)
+    u_11 = np.uint64(11)
 
     def __init__(self, state: np.ndarray):
         super().__init__('Xoshiro256+')
@@ -18,7 +21,7 @@ class Xoshiro256p(RNG):
     def next_uint(self) -> np.int64:
         result = self.state[1] + self.state[3]
 
-        t = self.state[1] << 17
+        t = self.state[1] << self.u_17
 
         self.state[2] ^= self.state[0]
         self.state[3] ^= self.state[1]
@@ -35,11 +38,12 @@ class Xoshiro256p(RNG):
 
     def next_float(self) -> np.float64:
         # Only take the upper 53 bits for floating points.
-        return (self.next_uint() >> 11) / Xoshiro256p.max_float
+        return (self.next_uint() >> self.u_11) / Xoshiro256p.max_float
 
     def next_64_bits(self) -> np.int64:
         return self.next_int()
 
     @staticmethod
-    def rol64(x: np.int64, k: int) -> np.int64:
-        return (x << k) | (x >> (64 - k))
+    def rol64(x: np.uint64, k) -> np.uint64:
+        return (x << np.uint64(k)) | (x >> np.uint64(64 - k))
+
--- python-implementation/src/pnrg/LFSRs/xoshiro256ss.py
+++ python-implementation/src/pnrg/LFSRs/xoshiro256ss.py
@@ -2,13 +2,19 @@ import numpy as np
 
 from pnrg import RNG
 
+np.seterr(over='ignore')
 
 class Xoshiro256ss(RNG):
     """
     Adapted from http://xoshiro.di.unimi.it/
     """
     # maximum value when calculating floating point values, as only the upper 53 bits are used.
-    max_float = (1 << 53 - 1)
+    max_float = (1 << 53)
+    # casted constants.
+    u_5 = np.uint64(5)
+    u_7 = np.uint64(7)
+    u_9 = np.uint64(9)
+    u_11 = np.uint64(11)
 
     def __init__(self, state: np.ndarray):
         super().__init__('Xoshiro256**')
@@ -16,9 +22,9 @@ class Xoshiro256ss(RNG):
         self.state = state.astype(np.uint64)
 
     def next_uint(self) -> np.int64:
-        result = Xoshiro256ss.rol64(self.state[1] * 5, 7) * 9
+        result = Xoshiro256ss.rol64(self.state[1] * self.u_5, self.u_7) * self.u_9
 
-        t = self.state[1] << 17
+        t = self.state[1] << np.uint64(17)
 
         self.state[2] ^= self.state[0]
         self.state[3] ^= self.state[1]
@@ -35,11 +41,12 @@ class Xoshiro256ss(RNG):
 
     def next_float(self) -> np.float64:
         # Only take the upper 53 bits for floating points.
-        return np.int64(self.next_uint() >> 11) / Xoshiro256ss.max_float
+        return (self.next_uint() >> self.u_11) / Xoshiro256ss.max_float
 
     def next_64_bits(self) -> np.int64:
         return self.next_int()
 
     @staticmethod
-    def rol64(x: np.int64, k: int) -> np.uint64:
-        return (x << k) | (x >> (64 - k))
+    def rol64(x: np.uint64, k) -> np.uint64:
+        return (x << np.uint64(k)) | (x >> np.uint64(64 - k))
+
