6c94b19124f696c7e1e5600d4408cd60af13f952
Ewan Gilligan <eg207@st-andrews.ac.uk>
2019-07-17 15:06:37 +0100

    added xorshift128+

---
+++ python-implementation/src/pnrg/LFSRs/xorshift128p.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from pnrg import RNG
+
+
+class Xorshift128p(RNG):
+
+    def __init__(self, seed1, seed2):
+        super().__init__('Xorshift128+')
+        self.a = np.int64(seed1)
+        self.b = np.int64(seed2)
+
+    def next_int(self) -> np.int64:
+        t = self.a
+        s = self.b
+        self.a = s
+        t ^= t << 23
+        t ^= t >> 17
+        t ^= (s >> 26)
+        self.b = t
+        return t + s
+
+    def next_float(self) -> np.float64:
+        return self.next_int() / np.iinfo(np.int64).max
+
+    def next_64_bits(self) -> np.int64:
+        return self.next_int()
--- python-implementation/src/pnrg/__init__.py
+++ python-implementation/src/pnrg/__init__.py
@@ -1,7 +1,7 @@
 
 from .RNG import RNG
 # from .LCGs import *
-# from .LFSR import *
+# from .LFSRs import *
 # from .CSG import *
 from .FromBinaryFile import FromBinaryFile
 from .pythonRandom import pythonRandom
