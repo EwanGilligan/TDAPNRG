d5f57b223a83d8ece782474f0106149a5c907202
Ewan Gilligan <eg207@st-andrews.ac.uk>
2019-07-17 14:39:51 +0100

    added salt to SHA based generators.

--- python-implementation/src/pnrg/hash/sha.py
+++ python-implementation/src/pnrg/hash/sha.py
@@ -6,14 +6,16 @@ from hashlib import sha256
 
 class SHA(RNG):
 
-    def __init__(self, hashobj, seed: np.int64):
-        super().__init__(hashobj.__name__)
-        self.hash = hashobj
+    def __init__(self, hash_obj, seed: np.int64, salt: str):
+        super().__init__(hash_obj.__name__)
+        self.hash_obj = hash_obj
         self.seed = np.int64(seed)
+        self.salt = salt.encode('utf-8')
 
     def next_int(self) -> np.int64:
         self.seed += 1
-        hashed_bytes = self.hash(bytes(self.seed))
+        hashed_bytes = self.hash_obj(bytes(self.seed))
+        hashed_bytes.update(self.salt)
         int_value = int.from_bytes(hashed_bytes.digest(), byteorder='little') % np.iinfo(np.int64).max
         return np.int64(int_value)
 
--- python-implementation/src/pnrg/hash/sha256.py
+++ python-implementation/src/pnrg/hash/sha256.py
@@ -3,5 +3,5 @@ from hashlib import sha256
 
 
 class SHA256(SHA):
-    def __init__(self, seed):
-        super().__init__(sha256, seed)
+    def __init__(self, seed, salt):
+        super().__init__(sha256, seed, salt)
