2c0201f7fa3993a2253faa90858f03ef7c482f76
Ewan Gilligan <eg207@st-andrews.ac.uk>
2019-07-18 11:57:47 +0100

    fixed xorshift128+ having negative floats.

--- python-implementation/src/pnrg/LFSRs/xorshift128p.py
+++ python-implementation/src/pnrg/LFSRs/xorshift128p.py
@@ -21,7 +21,7 @@ class Xorshift128p(RNG):
         return t + s
 
     def next_float(self) -> np.float64:
-        return self.next_int() / np.iinfo(np.int64).max
+        return abs(self.next_int()) / np.iinfo(np.int64).max
 
     def next_64_bits(self) -> np.int64:
         return self.next_int()
